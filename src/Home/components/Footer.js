86ed33a42d9ca10c3fb6e1a74142a855f2d11c56
spooby <39270883+sp0oby@users.noreply.github.com>
2022-04-17 06:32:19 -0400

    Update Footer.js
    update twitter

--- src/Home/components/Footer.js
+++ src/Home/components/Footer.js
@@ -19,7 +19,7 @@ export default function Footer() {
         </a>
       </Grid>
       <Grid item>
-        <a href="null" target="__blank">
+        <a href="https://twitter.com/kyriosmetis" target="__blank">
           <img src={twIcon} alt="" width={48} height={48} />
         </a>
       </Grid>
